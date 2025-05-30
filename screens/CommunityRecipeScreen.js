81222da2caa2cae331ce97cfa0b601bb3b7e34dd
Ryan <songjunseo13@gmail.com>
2025-05-29 21:09:20 -0700

    Update CommunityRecipeScreen.js

--- screens/CommunityRecipeScreen.js
+++ screens/CommunityRecipeScreen.js
@@ -1,13 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
+import { collection , getDocs, addDoc } from "firebase/firestore"; 
+import { db } from '../firebase';
+import { FlatList, SafeAreaView } from 'react-native-web';
+import { Item } from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
+
+const CommunityRecipeScreenState = () => {
+  const [todoList, setTodoList] = useState([]);
+
+    useEffect (()=>{
+        const readTodos = async () => {
+            const querySnapshot = await getDocs(collection(db, "book"));
+
+            setToDolist(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+            
+        }
+        readTodos()
+
+    }, [])
+
+  
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Top Ten Best Rated Dishes</Text>
+        <FlatList data = {setTodoList} renderItem={renderToDoItem} keyExtractor={Item}/>
+      </View>
+      
+    </SafeAreaView>
+  )
+
+}
 
 const CommunityRecipeScreen = () => {
   return (
     <View>
       <Text>CommunityRecipeScreen</Text>
+      
     </View>
   )
 }
+
+
 
 export default CommunityRecipeScreen
 
