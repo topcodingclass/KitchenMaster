4900e651e6635de481f5dc922d0d957734d2b623
Ryan <songjunseo13@gmail.com>
2025-06-24 19:07:51 -0700

    Update CommunityRecipeDetailScreen.js

--- screens/CommunityRecipeDetailScreen.js
+++ screens/CommunityRecipeDetailScreen.js
@@ -1,11 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, SafeAreaView,  } from 'react-native'
+import React, {useState, useLayoutEffect} from 'react'
+import { Text } from 'react-native-paper';
 
-const CommunityRecipeDetailScreen = () => {
+const CommunityRecipeDetailScreen = ({navigation,route}) => {
+const { recipe } = route.params;
+console.log(recipe)
+
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: recipe.name 
+    });
+  }, [navigation, recipe]);
+
+
   return (
-    <View>
-      <Text>CommunityRecipeDetailScreen</Text>
-    </View>
+    <SafeAreaView>
+      
+
+      <View>
+        <Text>{recipe.description}</Text>
+      </View>
+
+       <View style = {{margin: 10}}>
+      <Text variant="titleMedium">Ingredients:</Text>
+      {recipe.ingredients.map((item, index) => (
+        <Text key={index} >
+          {item.ingredient} - {item.quantity}
+        </Text>
+      ))}
+      </View>
+
+      <View>
+        <Text variant="titleMedium">Steps: </Text>
+        {recipe.steps.map((item,index) =>(
+          <Text key = {index}>
+            {item.sequence}-{item.description} - {item.time}
+
+          </Text>
+        ))}
+      </View>
+    </SafeAreaView>
+    
+    
+
+
   )
 }
 
