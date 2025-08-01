a7f4c6ba7a06becc97280730fed8051ea8caf637
deadlypants1607 <153317787+deadlypants1607@users.noreply.github.com>
2025-07-31 22:00:09 -0700

    Update StorageDetailScreen.js

--- screens/StorageDetailScreen.js
+++ screens/StorageDetailScreen.js
@@ -1,1 +1,15 @@
 
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StorageDetailScreen = () => {
+  return (
+    <View>
+      <Text>StorageDetailScreen</Text>
+    </View>
+  )
+}
+
+export default StorageDetailScreen
+
+const styles = StyleSheet.create({})
