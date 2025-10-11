f3c846fb6521d57fa41834bc164e6216622a86c6
deadlypants1607 <153317787+deadlypants1607@users.noreply.github.com>
2025-10-10 17:04:31 -0700

    Update FoodManualAddScreen.js

--- screens/FoodManualAddScreen.js
+++ screens/FoodManualAddScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Modal, FlatList, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Modal, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import { TextInput, Text, Button } from 'react-native-paper';
 import { addDoc, collection, getDocs, query, where, deleteDoc, Timestamp } from 'firebase/firestore';
 import { db } from '../firebase';
@@ -84,85 +84,92 @@ const FoodManualAddScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput label="Name" value={name} onChangeText={setName} mode="outlined" style={{ marginTop: 12 }} />
-      <TextInput label="Quantity" value={quantity} onChangeText={setQuantity} mode="outlined" style={{ marginTop: 12 }} />
-      <TextInput label="Expiration Date" value={expirationDate} onChangeText={setExpirationDate} mode="outlined" style={{ marginTop: 12 }} />
-      <TextInput label="Type" value={type} onChangeText={setType} mode="outlined" style={{ marginTop: 12 }} />
-      <TextInput label="Mass" value={mass} onChangeText={setMass} mode="outlined" style={{ marginTop: 12 }} />
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        
+        <TextInput label="Name" value={name} onChangeText={setName} mode="outlined" style={{ marginTop: 12 }} />
+        <TextInput label="Quantity" value={quantity} onChangeText={setQuantity} mode="outlined" style={{ marginTop: 12 }} />
+        <TextInput label="Expiration Date" value={expirationDate} onChangeText={setExpirationDate} mode="outlined" style={{ marginTop: 12 }} />
+        <TextInput label="Type" value={type} onChangeText={setType} mode="outlined" style={{ marginTop: 12 }} />
+        <TextInput label="Mass" value={mass} onChangeText={setMass} mode="outlined" style={{ marginTop: 12 }} />
 
-      <TextInput label="Calories" value={calories} onChangeText={setCalories} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
-      <TextInput label="Protein (g)" value={protein} onChangeText={setProtein} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
-      <TextInput label="Carbs (g)" value={carb} onChangeText={setCarb} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
-      <TextInput label="Fat (g)" value={fat} onChangeText={setFat} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
+        <TextInput label="Calories" value={calories} onChangeText={setCalories} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
+        <TextInput label="Protein (g)" value={protein} onChangeText={setProtein} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
+        <TextInput label="Carbs (g)" value={carb} onChangeText={setCarb} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
+        <TextInput label="Fat (g)" value={fat} onChangeText={setFat} mode="outlined" style={{ marginTop: 12 }} keyboardType="numeric" />
 
-      <Text variant="titleMedium" style={{ marginTop: 20 }}>Storage:</Text>
-      <Text variant="titleSmall" style={{ marginLeft: 60, marginBottom: 10 }}>{storage}</Text>
+        <Text variant="titleMedium" style={{ marginTop: 20 }}>Storage:</Text>
+        <Text variant="titleSmall" style={{ marginLeft: 60, marginBottom: 10 }}>{storage}</Text>
 
-      <Button icon="file-cabinet" mode="contained-tonal" onPress={() => setModalVisible(true)}>
-        Select Storage
-      </Button>
+        <Button icon="file-cabinet" mode="contained-tonal" onPress={() => setModalVisible(true)}>
+          Select Storage
+        </Button>
 
-      {/* Modal */}
-      <Modal visible={modalVisible} transparent animationType="slide">
-        <View style={styles.modalBackground}>
-          <View style={styles.modalContainer}>
-            <Text variant="titleMedium">Select Existing Storage</Text>
+        {/* Modal */}
+        <Modal visible={modalVisible} transparent animationType="slide">
+          <View style={styles.modalBackground}>
+            <View style={styles.modalContainer}>
+              <Text variant="titleMedium">Select Existing Storage</Text>
 
-            <FlatList
-              data={storages}
-              keyExtractor={(item) => item.value}
-              renderItem={({ item }) => (
-                <View style={styles.storageRow}>
-                  <TouchableOpacity
-                    style={styles.selectButton}
-                    onPress={() => {
-                      setStorage(item.value);
-                      setModalVisible(false);
-                    }}
-                  >
-                    <Text>{item.label}</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={styles.deleteButton}
-                    onPress={() => deleteStorage(item.value)}
-                  >
-                    <Text style={{ color: 'white' }}>Delete</Text>
-                  </TouchableOpacity>
-                </View>
-              )}
-            />
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20, alignItems: 'center', marginBottom: 20 }}>
-              <TextInput
-                placeholder="New Storage Name"
-                value={storageID}
-                onChangeText={setStorageID}
-                mode="outlined"
-                style={{ flex: 1, marginRight: 10 }}
+              <FlatList
+                data={storages}
+                keyExtractor={(item) => item.value}
+                renderItem={({ item }) => (
+                  <View style={styles.storageRow}>
+                    <TouchableOpacity
+                      style={styles.selectButton}
+                      onPress={() => {
+                        setStorage(item.value);
+                        setModalVisible(false);
+                      }}
+                    >
+                      <Text>{item.label}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.deleteButton}
+                      onPress={() => deleteStorage(item.value)}
+                    >
+                      <Text style={{ color: 'white' }}>Delete</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
               />
-              <Button icon="archive-plus-outline" mode="contained-tonal" onPress={addStorage}>
-                Add Storage
+
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20, alignItems: 'center', marginBottom: 20 }}>
+                <TextInput
+                  placeholder="New Storage Name"
+                  value={storageID}
+                  onChangeText={setStorageID}
+                  mode="outlined"
+                  style={{ flex: 1, marginRight: 10 }}
+                />
+                <Button icon="archive-plus-outline" mode="contained-tonal" onPress={addStorage}>
+                  Add Storage
+                </Button>
+              </View>
+
+              <Button mode="contained" style={styles.closeButton} onPress={() => setModalVisible(false)}>
+                Close
               </Button>
             </View>
-
-            <Button mode="contained" style={styles.closeButton} onPress={() => setModalVisible(false)}>
-              Close
-            </Button>
           </View>
-        </View>
-      </Modal>
+        </Modal>
 
-      <Button mode="contained" style={{ marginTop: 30 }} onPress={addFood}>
-        Add Food
-      </Button>
-    </View>
+        <Button mode="contained" style={{ marginTop: 30, marginBottom: 40 }} onPress={addFood}>
+          Add Food
+        </Button>
+      </View>
+    </ScrollView>
   );
 };
 
 export default FoodManualAddScreen;
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 80, 
+  },
   container: {
     flex: 1,
     padding: 10,
